9b4d34377abb437013515b532b66a38079ec848f
PrincessMav <princess-iza@hotmail.co.uk>
2021-12-03 14:30:41 +0000

    added user service class

---
+++ src/main/java/com/PrincessCyphers/Service/UserService.java
@@ -0,0 +1,19 @@
+package com.PrincessCyphers.Service;
+
+import com.PrincessCyphers.repository.UserRepository;
+import com.PrincessCyphers.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> listAll(){
+        return (List<User>) repository.findAll();
+    }
+
+}
