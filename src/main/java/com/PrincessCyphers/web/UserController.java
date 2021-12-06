aef07788e061017c7a557d304b22fa2c2236836f
PrincessMav <princess-iza@hotmail.co.uk>
2021-12-06 18:31:20 +0000

    added mapping for user editting with success message

--- src/main/java/com/PrincessCyphers/web/UserController.java
+++ src/main/java/com/PrincessCyphers/web/UserController.java
@@ -1,11 +1,13 @@
 package com.PrincessCyphers.web;
 
+import com.PrincessCyphers.Service.UserNotFoundException;
 import com.PrincessCyphers.Service.UserService;
 import com.PrincessCyphers.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -28,6 +30,7 @@ public class UserController {
 
     public String showNewForm(Model model) {
         model.addAttribute("user", new User());
+        model.addAttribute("pageTitle", "Add New User");
         return "user_form";
     }
 
@@ -37,5 +40,18 @@ public class UserController {
         ra.addFlashAttribute("message", "New user successfully added");
 
         return "redirect:/users";
+    }
+
+    @GetMapping("users/edit/{id}")
+    public String showEditForm(@PathVariable("id") Integer id, Model model, RedirectAttributes ra) {
+        try {
+            User user = service.get(id);
+            model.addAttribute("user", user);
+            model.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
+            return "user_form";
+        } catch (UserNotFoundException e) {
+            ra.addFlashAttribute("message", "User has been updated successfully");
+            return "redirect:/users";
+        }
     }
 }
